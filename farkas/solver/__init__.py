d1a33f377b2d4224bf5840896ecc37625291865f
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-04-01 22:41:15 +0200

    documentation

--- farkas/solver/__init__.py
+++ farkas/solver/__init__.py
@@ -5,6 +5,7 @@ problems from coefficient vectors and matrices.
 
 .. code-block::
 
+    import numpy as np
     from farkas.solver import MILP, LP, Wrapper
     wrapper = Wrapper("cbc")
     # example for a LP instance. optimal result should be x_opt=[1.5,7.0].
@@ -13,6 +14,22 @@ problems from coefficient vectors and matrices.
     opt = np.array([-1,-1])
     lp = LP(A,b,opt)
     result = wrapper.solve(lp)
+    print(result)
+
+.. code-block::
+
+    import numpy as np
+    from farkas.solver import MILP, LP, Wrapper
+    wrapper = Wrapper("cbc")
+    # example for a MILP instance. optimal result should be x_opt=[2,6].
+    # this is the same as the LP instance but with an added integer constraint for the first variable.
+    A = np.matrix([[2,1],[4,-1],[-8,2],[-1,0],[0,-1]])
+    b = np.array([10,8,2,0,0])
+    opt = np.array([-1,-1])
+    I = {0}
+    milp = MILP(A,b,opt,I)
+
+    result = wrapper.solve(milp)
     print(result)
 
 """
