acb1ad4aa6a3ea60b2c460f2800f65a4be0b67e7
Simon Jantsch <simon.jantsch@tu-dresden.de>
2020-04-07 09:15:50 +0200

    import also the induced_subsystems function

--- farkas/model/__init__.py
+++ farkas/model/__init__.py
@@ -1,4 +1,4 @@
-from .reachability_form import ReachabilityForm
+from .reachability_form import ReachabilityForm, induced_subsystem
 from .amdp import AbstractMDP
 from .dtmc import DTMC
 from .mdp import MDP
