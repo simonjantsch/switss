061b61cc11e10b9876e3aaaaa08a42428951dc53
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-04-13 14:33:57 +0200

    added (first) string representation to MILPExact class

--- farkas/problem/milpexact.py
+++ farkas/problem/milpexact.py
@@ -1,9 +1,13 @@
 from . import ProblemFormulation, ProblemResult
 
 class MILPExact(ProblemFormulation):
-    def __init__(self):
+    def __init__(self, solver):
         super().__init__()
+        self.solver = solver
 
     def solve(self, reachability_form):
         # model.ReachabilityForm -> problem.ProblemResult
         pass
+
+    def __repr__(self):
+        return "MILPExact(solver=%s)" % (self.solver)
