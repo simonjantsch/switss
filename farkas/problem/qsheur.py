e88a16b07e05cf99ec58a6fedffa52b77de0f9dc
Simon Jantsch <simon.jantsch@tu-dresden.de>
2020-04-14 10:26:25 +0200

    change parameter name for min/max from 'objective' into 'mode'

--- farkas/problem/qsheur.py
+++ farkas/problem/qsheur.py
@@ -17,17 +17,17 @@ class QSHeur(ProblemFormulation):
     where :math:`\mathbf{o}_0` is a vector of initial weights (:math:`(1,\ldots,1)` is the default) and :math:`\mathbf{o}_i = \operatorname{upd}(QS(i-1))` for a given update function :math:`\operatorname{upd}` (where pointwise :math:`1/x` if :math:`x \\neq 0`, and a big constant otherwise, is the default). """
     def __init__(self,
                  threshold,
-                 objective,
+                 mode,
                  iterations = 3,
                  initializer = AllOnesInitializer(),
                  updater = InverseResultUpdater(),
                  solver_name="cbc"):
         super().__init__()
-        assert objective in ["min","max"]
+        assert mode in ["min","max"]
         assert solver_name in ["gurobi","cbc"]
         assert (threshold >= 0) and (threshold <= 1)
 
-        self.objective = objective
+        self.mode = mode
         self.threshold = threshold
         self.iterations = iterations
         self.solver = solver_name
@@ -35,12 +35,12 @@ class QSHeur(ProblemFormulation):
         self.initializer = initializer
 
     def __repr__(self):
-        return "QSHeur(threshold=%s, objective=%s, solver=%s, iterations=%s, initializer=%s, updater=%s)" % (
-            self.threshold, self.objective, self.solver, self.iterations, self.initializer, self.updater)
+        return "QSHeur(threshold=%s, mode=%s, solver=%s, iterations=%s, initializer=%s, updater=%s)" % (
+            self.threshold, self.mode, self.solver, self.iterations, self.initializer, self.updater)
 
     def solve(self, reach_form):
-        """Runs the QSheuristic on the Farkas (min- or max-) polytope depending on the value in objective."""
-        if self.objective == "min":
+        """Runs the QSheuristic on the Farkas (min- or max-) polytope depending on the value in mode."""
+        if self.mode == "min":
             return self.solve_min(reach_form)
         else:
             return self.solve_max(reach_form)
@@ -49,7 +49,7 @@ class QSHeur(ProblemFormulation):
         """Runs the QSheuristic on the Farkas min-polytope of a given reachability form for a given threshold."""
         C,N = reach_form.P.shape
 
-        current_weights = self.initializer.initialize(reach_form, self.objective)
+        current_weights = self.initializer.initialize(reach_form, self.mode)
 
         # computes the constraints for the Farkas min-polytope of the given reachability form
         fark_matr,fark_rhs = reach_form.fark_min_constraints(self.threshold)
@@ -73,7 +73,7 @@ class QSHeur(ProblemFormulation):
 
                 yield ProblemResult("success", witness)
                 
-                current_weights = self.updater.update(heur_i_result.result, self.objective)
+                current_weights = self.updater.update(heur_i_result.result, self.mode)
             else:
                 # failed to optimize LP
                 yield ProblemResult(heur_i_result.status, None)
@@ -82,7 +82,7 @@ class QSHeur(ProblemFormulation):
         """Runs the QSheuristic on the Farkas max-polytope of a given reachability form for a given threshold."""
         C,N = reach_form.P.shape
 
-        current_weights = self.initializer.initialize(reach_form, self.objective)
+        current_weights = self.initializer.initialize(reach_form, self.mode)
 
         # computes the constraints for the Farkas max-polytope of the given reachability form
         fark_matr,fark_rhs = reach_form.fark_max_constraints(self.threshold)
@@ -104,7 +104,7 @@ class QSHeur(ProblemFormulation):
 
                 yield ProblemResult("success", witness)
 
-                current_weights = self.updater.update(heur_i_result.result, self.objective)
+                current_weights = self.updater.update(heur_i_result.result, self.mode)
 
             else:
                 # failed to optimize LP
