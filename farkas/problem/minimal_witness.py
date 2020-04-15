6c54779a156bd4cc341a3e71d20d7b41f138f519
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-04-15 11:59:24 +0200

    MinimalWitness visualization now with added min-max-scaled weight-indicator

--- farkas/problem/minimal_witness.py
+++ farkas/problem/minimal_witness.py
@@ -23,27 +23,50 @@ class MinimalWitness:
 
         return self.__subsystem_mask
 
+    @property
+    def is_quadratic(self):
+        return self.__system.P.shape[0] == self.__system.P.shape[1]
+
     def digraph(self):
         graph = Digraph()
         model = None
 
+        # apply min-max-scaling for better weight indication.
+        sa_weights_scaled = self.__state_action_weights
+        # 0-entries would distort the outcome
+        min_0_ignored = min(filter(lambda x:x>0, sa_weights_scaled))
+        sa_weights_scaled = (sa_weights_scaled - min_0_ignored)/(np.max(sa_weights_scaled) - min_0_ignored)
+
         def state_map(stateidx, labels):
             in_subsystem = (stateidx < len(self.subsystem_mask) and self.subsystem_mask[stateidx])
+            label = "State %d\n%s" % (stateidx,",".join(labels))
             color = color_from_hash( tuple([sorted(labels), in_subsystem]) )
-            
+            if self.is_quadratic and in_subsystem:
+                weight = sa_weights_scaled[stateidx]
+                # color = "gray%d" % int(100-weight*100)
+                label = "{}\nweight={:.3f}".format(label, weight)
+                
             return { "style" : "filled",  
                      "color" : color,  
-                     "label" : "State %d\n%s" % (stateidx,",".join(labels)) } 
+                     "label" : label } 
 
         def action_map(sourceidx, action, sourcelabels):
-            return { "node" : { "color" : "black",   
-                                "label" : str(action),  
+            in_subsystem = (sourceidx < len(self.subsystem_mask) and self.subsystem_mask[sourceidx])
+            color, label = "black", str(action)
+            if in_subsystem:
+                index = self.__system.index_by_state_action[(sourceidx, action)]
+                weight = sa_weights_scaled[index]
+                # color = "gray%d" % int(weight*100)
+                label = "{}\nweight={:.3f}".format(label, weight)
+                
+            return { "node" : { "color" : color,   
+                                "label" : label,
                                 "style" : "solid",   
                                 "shape" : "rectangle" },   
-                     "edge" : { "color" : "black",  
+                     "edge" : { "color" : color,  
                                 "dir" : "none" } }
 
-        if self.__system.P.shape[0] == self.__system.P.shape[1]:
+        if self.is_quadratic:
             model = DTMC.from_reachability_form(self.__system)
             graph = model.digraph(state_map=state_map)
         else:
