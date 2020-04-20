842f3423baeecb84ef99891378b9d41540932ef5
Simon Jantsch <simon.jantsch@tu-dresden.de>
2020-04-20 10:51:14 +0200

    docs for certification

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -42,9 +42,22 @@ Problem
    :members:
    :undoc-members:
 
+Certification
+=================================
+.. automodule:: farkas.certification
+   :imported-members:
+   :members:
+   :undoc-members:
+
 Indices and tables
 ==================
 
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
+References
+==================
+.. rubric:: References
+
+.. [FJB19] Funke, F; Jantsch, S; Baier, C: Farkas certificates and minimal witnessing subsystems for probabilistic reachability constraints. (https://arxiv.org/abs/1910.10636)
--- farkas/certification/certificates.py
+++ farkas/certification/certificates.py
@@ -2,6 +2,13 @@ from farkas.solver import LP
 import numpy as np
 
 def generate_farkas_certificate(reach_form, mode, sense, threshold,solver="cbc"):
+    """ Generates Farkas certificates for a given reachability form, mode, sense and threshold using the characterizations in Table 1 of [FJB19]_.
+    To this end uses an LP solver to find a satisfying vector of the corresponding polytope.
+    For strict inequalities, maximizes (minimizes) the probability and then checks whether the result value is strictly greater than the threshold.
+
+    As the solvers that are currently supported do not use precise arithmetic the resulting vectors may deviate from the desired result slightly.
+    """
+
     assert (threshold >= 0) and (threshold <= 1)
 
     farkas_matr,rhs = __get_right_constraint_set(reach_form,mode,sense,threshold)
@@ -52,7 +59,15 @@ def generate_farkas_certificate(reach_form, mode, sense, threshold,solver="cbc")
         print("Property is not satisfied!")
         return None
 
-def check_farkas_certificate(reach_form, mode, sense, threshold, farkas_vec, prec=9):
+def check_farkas_certificate(reach_form, mode, sense, threshold, farkas_vec, prec=5):
+    """Given a reachability form, mode, sense, threshold and candidate vector, checks
+    whether the vector is a legal Farkas certificate for the reachability constraint.
+
+    To cope with the problem of imprecise LP-solver results, the function allows to
+    define a precision :math:`prec` (the default value is 5).
+    It will then round the vector pointwise to :math:`prec` decimals and check the
+    certificate conditions afterwards.
+    """
 
     assert (threshold >= 0) and (threshold <= 1)
 
