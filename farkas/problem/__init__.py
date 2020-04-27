9c3611ae3c473a491336cf8ddb1226efb93e5a25
Simon Jantsch <simon.jantsch@tu-dresden.de>
2020-04-27 19:04:27 +0200

    refactored groups feature

--- farkas/problem/__init__.py
+++ farkas/problem/__init__.py
@@ -1,7 +1,6 @@
 from .subsystem import Subsystem
 from .problemresult import ProblemResult
 from .problemform import ProblemFormulation
-from .problem_utils import var_groups_program, project_from_binary_indicators, var_groups_from_state_groups
 from .milpexact import MILPExact
 from .qsheurparams import Initializer, Updater, AllOnesInitializer, InverseResultUpdater
 # from .qsheurparams import InverseReachabilityInitializer
