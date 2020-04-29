eefdf5fc4ce00c5eb7f3c4f9776d2614b7cf0b0f
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-04-29 16:24:26 +0200

    import from qsheurparams now concrete classes

--- farkas/problem/__init__.py
+++ farkas/problem/__init__.py
@@ -1,7 +1,6 @@
-from .qsheurparams import *
+from .qsheurparams import AllOnesInitializer, Initializer, InverseFrequencyInitializer, InverseReachabilityInitializer, InverseResultUpdater, Updater 
 from .problemform import ProblemFormulation
 from .subsystem import Subsystem
 from .problemresult import ProblemResult
 from .qsheur import QSHeur
 from .milpexact import MILPExact
-
