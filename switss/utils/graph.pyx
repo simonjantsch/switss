91969923fb07068c8b62b79c895709235715fd30
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-08-27 21:59:44 +0200

    added (untested) version of MEC-implementation for graphs

--- switss/utils/graph.pyx
+++ switss/utils/graph.pyx
@@ -4,6 +4,7 @@ from libc.stdlib cimport malloc, free
 import numpy as np
 from scipy.sparse import dok_matrix
 from bidict import bidict
+from abc import classmethod
 
 ctypedef (int,int,float) SAPPair
 
@@ -33,26 +34,59 @@ cdef void freestack(Stack* stack):
 cdef struct Node:
     SAPPair *predecessors
     SAPPair *successors
-    int predcount, succcount
+    int predcount, succcount 
 
 cdef struct TarjanNode:
     int index, lowlink
     int onstack
 
+cdef struct SubMDP:
+    int* E
+    SAPPair* F
+    int nodecount, sapcount
+
 cdef class Graph:
     cdef Node *nodes
     cdef int nodecount
 
-    def __cinit__(self, P, index_by_state_action):
-        self.nodecount = P.shape[1]
-        self.nodes = <Node *> malloc(self.nodecount * sizeof(Node))
+    def __cinit__(self, P=None, index_by_state_action=None):
+        if P is not None and index_by_state_action is not None:
+            self.nodecount = P.shape[1]
+            self.nodes = <Node *> malloc(self.nodecount * sizeof(Node))
+            for i in range(self.nodecount):
+                self.nodes[i] = Node(NULL,NULL,0,0)
+            for (i,d),p in P.items():
+                s,a = index_by_state_action.inv[i]
+                self.add_successor(s,a,p,d)
 
+    def subgraph(self, vmask):
+        # compute the size of the subgraph (=#vertices)
+        subgraphsize = 0
+        sub_to_sup = bidict() 
         for i in range(self.nodecount):
-            self.nodes[i] = Node(NULL,NULL,0,0)
+            if vmask[i] == 1: 
+                sub_to_sup[subgraphsize] = i
+                subgraphsize += 1
 
-        for (i,d),p in P.items():
-            s,a = index_by_state_action.inv[i]
-            self.add_successor(s,a,p,d)
+        cdef Node *nodes = <Node *> malloc(subgraphsize*sizeof(Node)) 
+        for i in range(subgraphsize):
+            nodes[i] = Node(NULL,NULL,0,0)
+        sgraph = Graph()
+        sgraph.nodes = nodes
+        sgraph.nodecount = subgraphsize
+
+        cdef SAPPair* successors
+        for i in range(subgraphsize):
+            orig = sub_to_sup[i]
+            removed_actions = set() # ignore all actions which have successors that are not masked
+            for d,a,p in self.successors(orig):
+                if vmask[d] == 0:
+                    removed_actions.add(a)
+            for d,a,p in self.successors(orig):
+                if a not in removed_actions:
+                    sgraph.add_successor(i,a,p,sub_to_sup.inv[d])
+
+        return sgraph, sub_to_sup
 
     cdef void add_successor(self, int fromidx, int action, float prob, int toidx):
         cdef Node* fromnode = &self.nodes[fromidx]
@@ -74,15 +108,17 @@ cdef class Graph:
         free(tonode[0].predecessors)
         tonode[0].predecessors = newpreds
 
-    def successors(self, nodeidx):
+    def successors(self, nodeidx, actionidx=None):
         cdef Node *node = &self.nodes[nodeidx]
         for i in range(node[0].succcount):
-            yield node[0].successors[i]
+            if actionidx is None or actionidx == node[0].successors[i][1]:
+                yield node[0].successors[i]
     
-    def predecessors(self, nodeidx):
+    def predecessors(self, nodeidx, actionidx=None):
         cdef Node *node = &self.nodes[nodeidx]
         for i in range(node[0].predcount):
-            yield node[0].predecessors[i]
+            if actionidx is None or actionidx == node[0].predecessors[i][1]:
+                yield node[0].predecessors[i]
 
     def __str__(self):
         cdef Node *currentnode
@@ -124,7 +160,6 @@ cdef class Graph:
 
     def strongly_connected_components(self):
         # Implementation of Tarjan's Algorithm
-
         # initialize vector containing strongly connected endcomponents
         cdef int* scs = <int *> malloc(self.nodecount * sizeof(int))
         cdef int sccount = 0
@@ -151,7 +186,36 @@ cdef class Graph:
         free(scs)
         freestack(stack)
         
-        return scs_arr
+        return scs_arr, sccount
+
+    def maximal_end_components(self):
+        # iteratively create sub-graphs. 
+        # but this may be a bit time-consuming, since it involves copying 
+        # quite a few nodes from graphs to subgraphs. 
+        # possible to share objects and instead just point to parent's objects? 
+        # this would avoid unneccessary copying...
+        # we should implement the copy-approach first; it's just simpler and we will
+        # see whether the algorithm really works.
+        ret = []
+        stack = [ (self,[]) ]
+        while len(stack) > 0:
+            graph,mappings = stack.pop()
+            components,compcount = graph.strongly_connected_components()
+            if compcount == 1:
+                # cannot be reduced much further,
+                # trace chain of mappings back to root graph 
+                states = mappings[-1].keys()
+                mapped_states = set()
+                for mapping in mappings[::-1]:
+                    for state in states:
+                        mapped_states.add(mapping[state])
+                    states = mapped_states
+                    mapped_states = set()
+                yield states
+            else:
+                sgs = [ graph.subgraph(components == i) for i in range(compcount) ]
+                stack += [ (subgraph ,mappings + [to_sup]) for subgraph,to_sup in sgs ]
+
 
     def reachable(self, fromset, direction, blocklist=set()):
         assert len(fromset) > 0
