21c8fedd1346f26e7de50df98cb52cbefe23b20d
Hans Harder <hans.harder@mailbox.tu-dresden.de>
2020-08-28 11:33:17 +0200

    documentation for new methods. also fixed a doc-compiler error

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+from setuptools import Extension
+
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
+ext_modules = [Extension("switss/utils/graph", ["switss/utils/graph.pyx"])]
+for e in ext_modules:
+    e.cython_directives = {"embedsignature": True}
+    
 setup(
     name = 'switss',
     version = '0.1',
     packages = find_packages(),
     scripts=['bin/switss'],
-    ext_modules = cythonize("switss/utils/graph.pyx"),
+    ext_modules = ext_modules,
     install_requires=[
         "graphviz",
         "scipy",
