7729e4f728b796b4c5c5f5d194aed191f69eaa6f
Simon Jantsch <simon.jantsch@tu-dresden.de>
2022-01-12 10:50:23 +0100

    removed unreachable code

--- switss/utils/__init__.py
+++ switss/utils/__init__.py
@@ -5,5 +5,3 @@ from .graph import Graph
 from .treaps import treap_testcases
 
 from .graph_utils import underlying_graph, quotient
-
-#from .tree_decompositions import fully_directed_tree_decomp, tree_decomp_from_partition_networkx, tree_decomp_from_partition_graphtool
